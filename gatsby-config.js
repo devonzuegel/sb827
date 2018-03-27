6b07e3c58c8f84c5ac8ec45b83218c267c72599c
Devon Kristine Zuegel <devonzuegel@gmail.com>
2018-03-26 18:23:41 -0700

    feat: Add heart favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,25 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     // Add typescript stack into webpack
-    `gatsby-plugin-typescript`
+    `gatsby-plugin-typescript`,
+
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
   ],
 }
