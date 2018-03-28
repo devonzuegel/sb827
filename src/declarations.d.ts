c0140bb401f76f9c04b1b3d8be832f4f57709950
Devon Kristine Zuegel <devonzuegel@gmail.com>
2018-03-28 11:19:03 -0700

    feat: Add support for importing JPGs

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,3 @@
 declare const graphql: (query: TemplateStringsArray) => void
+
+declare module '*.jpg'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
-import Link, {withPrefix} from 'gatsby-link'
+import Link from 'gatsby-link'
 import {BeforeAndAfter} from '../components/BeforeAndAfter'
 import {PlaceInfo} from '../components/PlaceInfo'
-const Render05Before = withPrefix(require('../images/v05-photo_L.jpg'))
-const Render05After = withPrefix(require('../images/v05-2_L.jpg'))
-const Render06Before = withPrefix(require('../images/v06_photo.jpg'))
-const Render06After = withPrefix(require('../images/v06.jpg'))
-const Render08Before = withPrefix(require('../images/v08-photo_L.jpg'))
-const Render08After = withPrefix(require('../images/v08_L.jpg'))
+import * as Render05Before from '../images/v05-photo_L.jpg'
+import * as Render05After from '../images/v05-2_L.jpg'
+import * as Render06Before from '../images/v06_photo.jpg'
+import * as Render06After from '../images/v06.jpg'
+import * as Render08Before from '../images/v08-photo_L.jpg'
+import * as Render08After from '../images/v08_L.jpg'
 
 // Please note that you can use https://github.com/dotansimha/graphql-code-generator
 // to generate all types from graphQL schema
@@ -38,8 +38,7 @@ export default class extends React.Component<IndexPageProps, {after: number}> {
         <PlaceInfo
           before={Render08Before}
           after={Render08After}
-          description="Loreimport { withPrefix } from 'gatsby-link'
-m ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d840.1963474816608!2d-117.084435970752!3d32.61190229881392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d94dd41d76dcf9%3A0xf2c2045a19309578!2s1041+Jefferson+Ave%2C+Chula+Vista%2C+CA+91911!5e0!3m2!1sen!2sus!4v1522210849364"
           address={['1041 Jefferson Ave', 'Chula Vista, CA 91911']}
         />
