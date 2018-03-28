ffd7a7ebb269ec1ed378e15d57aea4f6520343d2
Devon Kristine Zuegel <devonzuegel@gmail.com>
2018-03-28 12:40:04 -0700

    Updates

--- path---index-f10bd912b3a044b2710c.js
+++ path---index-f10bd912b3a044b2710c.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x81b8806e4260],{308:function(t,e){t.exports={data:{site:{siteMetadata:{title:"SB 827"}}},pathContext:{}}}});
+webpackJsonp([0x81b8806e4260],{319:function(t,e){t.exports={data:{site:{siteMetadata:{title:"SB 827"}}},pathContext:{}}}});
 //# sourceMappingURL=path---index-f10bd912b3a044b2710c.js.map
