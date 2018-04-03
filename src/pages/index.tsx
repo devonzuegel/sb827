8f2ebd5af3a96a2a11ee5a4a27ea267ff8344c6e
Devon Kristine Zuegel <devonzuegel@gmail.com>
2018-04-02 17:00:08 -0700

    feat: Add first set of images (7 total right now)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,14 @@ import {Introduction} from '../components/Introduction'
 import {ThankYou, AboutProject} from '../components/Footer'
 
 // Renders
+import * as Render01Before from '../images/1-before.jpg'
+import * as Render01After from '../images/1-after.jpg'
+import * as Render02Before from '../images/2-before.jpg'
+import * as Render02After from '../images/2-after.jpg'
+import * as Render03Before from '../images/3-before.jpg'
+import * as Render03After from '../images/3-after.jpg'
+import * as Render04Before from '../images/4-before.jpg'
+import * as Render04After from '../images/4-after.jpg'
 import * as Render05Before from '../images/5-before.jpg'
 import * as Render05After from '../images/5-after-B.jpg'
 import * as Render06Before from '../images/6-before.jpg'
@@ -36,11 +44,6 @@ interface IndexPageProps {
 }
 
 export default class extends React.Component<IndexPageProps, {after: number}> {
-  state = {after: 80}
-  constructor(props: IndexPageProps, context: any) {
-    super(props, context)
-  }
-
   public render() {
     return (
       <div>
@@ -48,7 +51,7 @@ export default class extends React.Component<IndexPageProps, {after: number}> {
           <h1>What would SB 827 mean for California?</h1>
           <Introduction />
         </div>
-        <BeforeAndAfter before={Render08Before} after={Render08After} percent={65} />
+        <BeforeAndAfter before={Render08Before} after={Render08After} percent={70} />
         <PlaceInfo
           before={Render08Before_FullSize}
           after={Render08After_FullSize}
@@ -61,7 +64,7 @@ export default class extends React.Component<IndexPageProps, {after: number}> {
           address={['1041 Jefferson Ave', 'Chula Vista, CA 91911']}
         />
 
-        <BeforeAndAfter before={Render06Before} after={Render06After} percent={45} />
+        <BeforeAndAfter before={Render06Before} after={Render06After} percent={65} />
         <PlaceInfo
           before={Render06Before_FullSize}
           after={Render06After_FullSize}
@@ -74,7 +77,7 @@ export default class extends React.Component<IndexPageProps, {after: number}> {
           address={['310 Wilson St', 'Petaluma, CA 94952']}
         />
 
-        <BeforeAndAfter before={Render05Before} after={Render05After} percent={60} />
+        <BeforeAndAfter before={Render05Before} after={Render05After} percent={70} />
         <PlaceInfo
           before={Render05Before_FullSize}
           after={Render05After_FullSize}
@@ -83,6 +86,14 @@ export default class extends React.Component<IndexPageProps, {after: number}> {
           src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d793.1359663801818!2d-121.9107603!3d37.329631!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fcb5cf5d99191%3A0x989323c608a07f0c!2s1030+Garland+Ave%2C+San+Jose%2C+CA+95126!5e0!3m2!1sen!2sus!4v1522212783283"
           address={['310 Wilson St', 'Petaluma, CA 94952']}
         />
+        <br />
+        <BeforeAndAfter before={Render01Before} after={Render01After} percent={65} />
+        <br />
+        <BeforeAndAfter before={Render02Before} after={Render02After} percent={75} />
+        <br />
+        <BeforeAndAfter before={Render03Before} after={Render03After} percent={60} />
+        <br />
+        <BeforeAndAfter before={Render04Before} after={Render04After} percent={65} />
         <div className="section">
           <b>
             TODO: Add 4th image + "SB 827 means vibrant, beautiful neighborhoods",
