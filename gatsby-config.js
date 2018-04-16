fb6cabc59a473e0c90807a73b09a44b07dce6b81
Devon Kristine Zuegel <devonzuegel@gmail.com>
2018-04-16 11:48:29 +0200

    feat: Add Google Analytics tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,5 +23,14 @@ module.exports = {
         },
       },
     },
+
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-65875472-3',
+        // Puts tracking script in the head instead of the body
+        head: false,
+      },
+    },
   ],
 }
