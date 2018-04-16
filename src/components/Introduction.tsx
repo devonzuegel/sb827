0db90f69fbe804f43152afde4d79d14b5a0f4850
John Backus <johncbackus@gmail.com>
2018-04-16 02:04:48 +0200

    Reduce wordiness of intro

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,96 +1,60 @@
-import * as React from 'react'
+import * as React from "react";
 
-import {ExternalLink} from '../components/Links'
+import { ExternalLink } from "../components/Links";
 
 export const Introduction = () => (
   <div className="two-columns">
     <p>
-      <i>
-        To get an intuitive sense of what SB 827 would make possible, some friends
-        and I created renderings to illustrate its potential. We picked a few vacant
-        but promising lots located near transit throughout the state. In a few years
-        they could be a new home for some friendly new neighbors. Use the sliders
-        below to see what they could become!
-      </i>
-    </p>
-    <hr />
-    <p>
-      But first, some background: California is in a housing crisis. For a sense of
-      its scale:
+      California is one of the most beautiful parts of the country and our
+      economy is booming. A lot of people want to experience our state's
+      opportunities but can't because of a shortage of places to live.
     </p>
     <ul>
       <li>
         California added 334 thousand residents in 2017, while only 88 thousand
-        residences were built in that same period. That's roughly{' '}
+        residences were built in that same period. That's roughly{" "}
         <ExternalLink link="www.dof.ca.gov/Forecasting/Demographics/Estimates/E-5/">
           one home for every four people
-        </ExternalLink>{' '}
+        </ExternalLink>{" "}
         who've come to the state, and this pattern has held for decades.
       </li>
       <li>
-        54% of renter households and 39% of mortgage holders paid over{' '}
+        54% of renter households and 39% of mortgage holders paid over{" "}
         <ExternalLink link="calbudgetcenter.org/resources/californians-parts-state-pay-can-afford-housing">
           30% of their income toward housing
-        </ExternalLink>{' '}
+        </ExternalLink>{" "}
         in 2015, and housing costs exceeded 50% of their income for half of each
         group.
       </li>
     </ul>
     <p>
-      None of this will come as a surprise if you’ve spent any time in California, so
-      I won't belabor the point. If you do want to learn more, you can find a great
-      primer on the subject{' '}
-      <ExternalLink link="calbudgetcenter.org/resources/californians-parts-state-pay-can-afford-housing">
-        here
-      </ExternalLink>.
-    </p>
-    <p>
-      We're <i>lucky</i> that people want to move here. California's economy is
-      booming, and some of the most beautiful places in the county are right in our
-      backyard. Newcomers want to participate in the opportunities available in our
-      state, but a shortage of places to live makes it hard for them to make those
-      contributions.
-    </p>
-    <p>
-      This is not a new problem, but{' '}
-      <b>
-        for the first time there's a proposal facing the state legislature that could
-        make a dent: Senate Bill 827
-      </b>, or{' '}
-      <ExternalLink link="leginfo.legislature.ca.gov/faces/billTextClient.xhtml?bill_id=201720180SB827">
-        SB 827
-      </ExternalLink>{' '}
-      for short. The bill would up-zone areas near transit to allow for mid-rise
-      housing.
+      Senate Bill 827 (<b>
+        <ExternalLink link="leginfo.legislature.ca.gov/faces/billTextClient.xhtml?bill_id=201720180SB827">
+          SB 827
+        </ExternalLink>
+      </b>) could change this! SB 827 would change zoning around transit to
+      allow for mid-rise housing.
     </p>
     <p>At a high level, the measure would:</p>
     <ul>
+      <li>Allow more houses to be built near transit stations,</li>
+      <li>Block city parking minimums that waste valuable space, and</li>
       <li>
-        Eliminate many restrictions on the number of houses allowed to be built near
-        transit stations,
-      </li>
-      <li>Block cities from imposing parking minimums, and</li>
-      <li>
-        Set minimum height limits ranging from 45 to 85 streets in places near
-        transit.
+        Set minimum height limits from 45 to 85 feet in places near transit.
       </li>
     </ul>
     <p>
-      I've seen countless scary-sounding headlines like "<ExternalLink link="padailypost.com/2018/02/18/housing-bill-result-85-foot-tall-buildings-el-camino">
+      This an seem scary at first ("<ExternalLink link="padailypost.com/2018/02/18/housing-bill-result-85-foot-tall-buildings-el-camino">
         Housing bill could result in 85-foot tall buildings on El Camino
-      </ExternalLink>". I totally understand where these fears come from—I grew up
-      around there, and it's frightening to think about how the places I love might
-      change.
+      </ExternalLink>"). I totally understand where these fears come from — I
+      grew up around there, and it's frightening to think about how the places I
+      love might change.
     </p>
     <p>
-      However, when I dug into the numbers, not only were my fears alleviated but I
-      actually got really excited. Rather than the community-destroying godzilla it's
-      been made out to be, SB 827 will make neighborhoods far more vibrant,
-      eco-friendly, and equitable.{' '}
-    </p>
-    <p>
-      I hope the renderings and perspectives below give you inspiration of how
-      California could change for the better under this bill!
+      Digging into the details convinced me how that SB 827 really is a force
+      for good. SB 827 makes neighborhoods more vibrant, equitable, and
+      eco-friendly! I walked away inspired by the opportunity this bill brings.
+      I hope these rendering and perspectives below inspire you.
     </p>
   </div>
-)
+);
